939d260b52989e4644005ada2125a8d2ce243918
ajitdangiiiitianwork-rgb <ajitdangi.iiitian.work@gmail.com>
2026-03-01 12:47:23 +0530

    1168-DUPLICATE-ZEROS Stats: Time: 0 ms (100%), Space: 14.4 MB (9.92%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.cpp
+++ 1168-duplicate-zeros/1168-duplicate-zeros.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
-    void Modify(vector<int>& arr, int idx){
-        int n = arr.size();
-        int i = n-1;
-        while(i != idx){
-            arr[i] = arr[i-1];
-            i--;
-        }
-        arr[i] = 0;
-    }
-
     void duplicateZeros(vector<int>& arr) {
         int n = arr.size();
-        int i = 0;
-        while(i < n-1){
-            if(arr[i] == 0){
-                Modify(arr, i+1);
-                i += 2;
-            } else i++;
+        vector<int> b;
+        for(int i = 0; i < n; i++){
+            if(b.size() == n) break;
+            b.push_back(arr[i]);
+            if(b.size() == n) break;
+            if(arr[i] == 0) b.push_back(arr[i]);
         }
+
+        arr = b;
     }
 };
