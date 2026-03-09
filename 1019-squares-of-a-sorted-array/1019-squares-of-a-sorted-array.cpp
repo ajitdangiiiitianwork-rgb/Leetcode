66d5f37fe798bc6ec23793369a627c2afcce3aff
ajitdangiiiitianwork-rgb <ajitdangi.iiitian.work@gmail.com>
2026-03-09 14:42:07 +0530

    1019-SQUARES-OF-A-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 30.4 MB (36.65%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for(int i = 0; i < nums.size(); i++){
-            nums[i] *= nums[i];
+        int val = 0;
+        int l = 0, r = nums.size()-1;
+        vector<int> result(nums.size(), 0);
+        for(int i = nums.size() - 1; i >= 0; i--){
+            if(abs(nums[l]) > abs(nums[r])) {
+                val = nums[l];
+                l++;
+            }
+            else {
+                val = nums[r];
+                r--;
+            }
+            result[i] = val*val;
         }
-        sort(nums.begin(), nums.end());
-        return nums;
+        return result;
     }
 };
