2af66d7a8be988e2199c58a0bd63a1c7047ff519
ajitdangiiiitianwork-rgb <ajitdangi.iiitian.work@gmail.com>
2026-04-10 21:54:52 +0530

    0051-N-QUEENS Stats: Time: 12 ms (9.65%), Space: 14.1 MB (9.57%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,54 +1,55 @@
 class Solution {
 public:
-    vector<vector<char>> grid;
-    vector<vector<string> > result;
-    vector<vector<string>>  solveNQueens(int n){
-    grid.clear();
-    result.clear();
-    grid.resize(n, vector<char>(n, '.')); 
-    f(0, n);
-    return result;
-    }
-    bool canPlaceQueen(int row, int col, int n){
-    //column check
-    for(int i = row-1; i >= 0; i--){
-        if(grid[i][col] == 'Q') return  false;
-    }
+    void backtrack(int row, unordered_set<int>& cols, unordered_set<int>& diags, unordered_set<int>& antidi,
+    vector<vector<string>>& boards, int n, vector<vector<char>>& game){
+        if(row == n){
+            // add game to boards
+            vector<string> temp;
+            for(int i = 0; i < n; i++){
+                string s = "";
+                for(int j = 0; j < n; j++){
+                    s += game[i][j];
+                }
 
-    // left diag check
-    for(int i = row-1, j = col-1; i >= 0 && j >= 0; i--, j--){
-        if(grid[i][j] == 'Q') return false;
-    }
+                temp.push_back(s);
+            }
 
-    //right diag check
-    for(int i = row-1, j = col+1; i>= 0 && j < n; i--, j++){
-        if(grid[i][j] == 'Q') return false;
-    }
-    return true;
-    }
-
-    void f(int row, int n){
-    if(row == n){
-        // We got one possible solution
-        vector<string> temp;
-        for(int i = 0; i < n; i++){
-        string res = "";
-        for(int j = 0; j < n; j++){
-            res += grid[i][j];
+            boards.push_back(temp);
+            return;
         }
-        temp.push_back(res);
-        }
-        result.push_back(temp); 
-        return;
-    }
-    for(int col = 0; col < n; col++){
-        if(canPlaceQueen(row, col, n)){
-        grid[row][col] = 'Q';
-        f(row+1, n);
-        grid[row][col] = '.';
+
+        for(int col = 0; col < n; col++){
+            if(cols.find(col) != cols.end()) continue;
+
+            int dia = col - row;
+            if(diags.find(dia) != diags.end()) continue;
+
+            int anti = col + row;
+            if(antidi.find(anti) != antidi.end()) continue;
+
+            cols.insert(col);
+            diags.insert(dia);
+            antidi.insert(anti);
+
+            game[row][col] = 'Q';
+            backtrack(row + 1, cols, diags, antidi, boards, n, game);
+            game[row][col] = '.';
+
+            cols.erase(col);
+            diags.erase(dia);
+            antidi.erase(anti);
         }
     }
+
+    vector<vector<string>> solveNQueens(int n){
+        unordered_set<int> cols;
+        unordered_set<int> diags;
+        unordered_set<int> antidi;
+
+        vector<vector<string>> boards;
+        vector<vector<char>> game(n, vector<char>(n, '.'));
+
+        backtrack(0, cols, diags, antidi, boards, n, game);
+        return boards;
     }
-
-
 };
