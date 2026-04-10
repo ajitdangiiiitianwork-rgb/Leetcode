a8b64684fcfda6583ce2b637cf68c58e18f3141e
ajitdangiiiitianwork-rgb <ajitdangi.iiitian.work@gmail.com>
2026-04-10 07:47:30 +0530

    0240-SEARCH-A-2D-MATRIX-II Stats: Time: 52 ms (65.8%), Space: 18.8 MB (37.6%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    bool binarySearch(vector<int>& arr, int start, int end, int target){
-        while(start <= end){
-            int mid = start + (end - start) / 2;
-            if(arr[mid] == target) return true;
-
-            if(arr[mid] > target) end = mid - 1;
-            else start = mid + 1;
-        }
-
-        return false;
-    }
-
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
         
-        for(int i = 0; i < n; i++){
-            bool flag = binarySearch(matrix[i], 0, m-1, target);
-            if(flag) return flag;
-        }
+        int row = 0, col = m-1;
+        while(row < n && col >= 0){
+            if(matrix[row][col] == target) return true;
 
+            else if(target < matrix[row][col]) col--; // eliminate that whole column
+
+            else row++;
+
+        }
         return false;
     }
 };
