8b0d2429dcb22be9af6e34d08667af2749878d05
ajitdangiiiitianwork-rgb <ajitdangi.iiitian.work@gmail.com>
2026-04-12 11:53:41 +0530

    0046-PERMUTATIONS Stats: Time: 0 ms (100%), Space: 10.6 MB (46.42%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    void helper(vector<vector<int>>& res, vector<int>& ans, vector<int> nums, int n){
+    void helper(vector<vector<int>>& res, vector<int>& nums, int idx){
         // Base case
-        if(ans.size() == n){
-            res.push_back(ans);
+        if(idx == nums.size()){
+            res.push_back(nums);
             return;
         }
-        for(int i = 0; i < nums.size(); i++){
-            ans.push_back(nums[i]);
-            vector<int> temp = nums;
-            temp.erase(temp.begin() + i);
-            helper(res, ans, temp, n);
-            ans.pop_back();
+        
+        for(int j = idx; j < nums.size(); j++){
+            swap(nums[idx], nums[j]);
+            helper(res, nums, idx + 1);
+            swap(nums[idx], nums[j]);
         }
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
-        vector<int> ans;
-        helper(res, ans, nums, nums.size());
+        helper(res,nums, 0);
         return res;
     }
 };
