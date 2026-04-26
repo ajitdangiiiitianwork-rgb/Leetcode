a20593def72658ee67a388119ab2e6a4a536e727
ajitdangiiiitianwork-rgb <ajitdangi.iiitian.work@gmail.com>
2026-04-26 10:11:33 +0530

    0731-MY-CALENDAR-II Stats: Time: 113 ms (20.47%), Space: 44.1 MB (41.51%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.cpp
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -1,37 +1,25 @@
 class MyCalendarTwo {
 public:
-    vector<vector<int>> bookings;
-    vector<vector<int>> overlapBookings;
+    map<int,int> counts;
 
     MyCalendarTwo() {
     
     }
     
     bool book(int startTime, int endTime) {
-        if(bookings.size() == 0){
-            bookings.push_back({startTime, endTime - 1});
-            return true;
-        }
+        counts[startTime]++;
+        counts[endTime]--;
 
-        // check if the interval is overlapping witn any interval in overlappingBookings -> three overlap intervals
-        for(int i = 0; i < overlapBookings.size(); i++){
-            if(endTime - 1 >= overlapBookings[i][0] && overlapBookings[i][1] >= startTime){
-                // intersection is there -> three overlapping intervals
+        // LSA algo
+        int bookings = 0;
+        for(auto const& p : counts){
+            bookings += p.second;
+            if(bookings > 2){
+                counts[startTime]--;
+                counts[endTime]++;
                 return false;
             }
         }
-
-        for(int i = 0; i < bookings.size(); i++){
-            if(endTime - 1 >= bookings[i][0] && bookings[i][1] >= startTime){
-                // intersection with interval in bookings
-                // add in overlapBookings
-                int s = max(startTime, bookings[i][0]);
-                int e = min(endTime-1, bookings[i][1]);
-                overlapBookings.push_back({s,e});
-            }
-        }
-
-        bookings.push_back({startTime, endTime-1});
         return true;
     }
 };
